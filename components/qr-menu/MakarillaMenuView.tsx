7ad665c86e4f3ad3a1af0563a3452e6be5799a15
ibrahimkaynar1979-creator <ibrahimkaynar1979@gmail.com>
2026-09-15 01:16:23 +0300

    Use Wrap banner in Makarilla category hero

--- components/qr-menu/MakarillaMenuView.tsx
+++ components/qr-menu/MakarillaMenuView.tsx
@@ -17,6 +17,7 @@ const categoryImages: Record<string, string> = {
 const categoryHeroImages: Record<string, string> = {
   pastas: '/makarilla_tabak_v2.png',
   'chicken-pastas': '/tavuklar_banner.png',
+  wraps: '/wrap_banner.png',
 };
 
 const categoryOrder = ['pastas', 'chicken-pastas', 'wraps', 'salads', 'desserts', 'drinks'];
