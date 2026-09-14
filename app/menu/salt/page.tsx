8177bdf79204da400bd7231cecd713621b7ab5c6
ibrahimkaynar1979-creator <ibrahimkaynar1979@gmail.com>
2026-09-14 15:13:22 +0300

    Apply exact SALT reference stylesheet

--- app/menu/salt/page.tsx
+++ app/menu/salt/page.tsx
@@ -1,5 +1,6 @@
 import { QrMenuApp } from '../../../components/qr-menu/QrMenuApp';
 import { saltRestaurant } from '../../../lib/qr-menu/salt';
+import './salt-reference.css';
 
 export default function SaltMenuPage() {
   return <QrMenuApp restaurant={saltRestaurant} />;
