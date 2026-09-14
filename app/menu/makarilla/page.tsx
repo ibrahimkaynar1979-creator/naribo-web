a63bba5ea76ad74f698a8972c40979fa5d404191
ibrahimkaynar1979-creator <ibrahimkaynar1979@gmail.com>
2026-09-14 23:53:42 +0300

    Route Makarilla menu button to category view

--- app/menu/makarilla/page.tsx
+++ app/menu/makarilla/page.tsx
@@ -1,6 +1,9 @@
+import { MakarillaMenuView } from '../../../components/qr-menu/MakarillaMenuView';
 import { SaltLanding } from '../../../components/qr-menu/SaltLanding';
 import { makarillaRestaurant } from '../../../lib/qr-menu/makarilla';
 
-export default function MakarillaMenuPage() {
+export default async function MakarillaMenuPage({ searchParams }: { searchParams: Promise<{ view?: string }> }) {
+  const params = await searchParams;
+  if (params.view === 'menu') return <MakarillaMenuView />;
   return <SaltLanding restaurant={makarillaRestaurant} />;
 }
