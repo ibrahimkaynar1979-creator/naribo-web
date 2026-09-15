2268fbbebea7c4f699c669af17d0ecc74a208b1c
ibrahimkaynar1979-creator <ibrahimkaynar1979@gmail.com>
2026-09-15 14:05:27 +0300

    Pass originating category to product detail

--- app/menu/makarilla/urun/[productId]/page.tsx
+++ app/menu/makarilla/urun/[productId]/page.tsx
@@ -1,6 +1,7 @@
 import { MakarillaProductDetail } from '../../../../../components/qr-menu/MakarillaProductDetail';
 
-export default async function MakarillaProductPage({ params }: { params: Promise<{ productId: string }> }) {
+export default async function MakarillaProductPage({ params, searchParams }: { params: Promise<{ productId: string }>; searchParams: Promise<{ from?: string }> }) {
   const { productId } = await params;
-  return <MakarillaProductDetail productId={productId} />;
+  const { from } = await searchParams;
+  return <MakarillaProductDetail productId={productId} fromCategory={from} />;
 }
