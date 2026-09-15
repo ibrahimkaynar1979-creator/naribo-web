f80f25767bdd962a8cbd6468b678a551dc2dc428
ibrahimkaynar1979-creator <ibrahimkaynar1979@gmail.com>
2026-09-15 14:04:55 +0300

    Preserve Makarilla menu category from query

--- app/menu/makarilla/page.tsx
+++ app/menu/makarilla/page.tsx
@@ -2,8 +2,8 @@ import { MakarillaMenuView } from '../../../components/qr-menu/MakarillaMenuView
 import { SaltLanding } from '../../../components/qr-menu/SaltLanding';
 import { makarillaRestaurant } from '../../../lib/qr-menu/makarilla';
 
-export default async function MakarillaMenuPage({ searchParams }: { searchParams: Promise<{ view?: string }> }) {
+export default async function MakarillaMenuPage({ searchParams }: { searchParams: Promise<{ view?: string; category?: string }> }) {
   const params = await searchParams;
-  if (params.view === 'menu') return <MakarillaMenuView />;
+  if (params.view === 'menu') return <MakarillaMenuView initialCategory={params.category} />;
   return <SaltLanding restaurant={makarillaRestaurant} />;
 }
